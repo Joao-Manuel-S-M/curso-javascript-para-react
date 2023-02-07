4cf31c1a6cd8437463854b4ba40ffd9ab5f19b4d
Joao-Manuel-S-M <jmsmarques8213@gmail.com>
2023-02-07 16:03:54 -0300

    :construction: Create BoardGame object without CSS

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,11 +1,18 @@
 import "./style.css";
 
-function CardGame () {
-    return `
-        <article class="card-game">
+
+
+function CardGame() {
+    
+    return`
+        <article class="card-game" id="card1">
             <img src="images/alura-pixel.png" alt="Logo da Alura">
         </article>
+     
     ` 
+    
 }
 
+
 export default CardGame;
+
